c8518ba3ea4e83a31008f8879c4f14a12733fabf
natha <natha@NATHALIALO>
2025-12-11 15:25:51 -0300

    teste

--- FCGProj/obj/Debug/net9.0/FCGProj.AssemblyInfo.cs
+++ FCGProj/obj/Debug/net9.0/FCGProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCGProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cf10fdd68e53735cb34e054c67b38a4177f9f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b3cf833532db4c34f4e2b31a1b02d09930405d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCGProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCGProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
